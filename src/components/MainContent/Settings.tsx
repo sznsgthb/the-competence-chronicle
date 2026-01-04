583af8a7055f2f57e34ea59db0a62c6d53efc0b2
sznsgthb <suzannehoftijzer@gmail.com>
2026-01-04 22:44:32 +0100

    bug fix

--- src/components/MainContent/Settings.tsx
+++ src/components/MainContent/Settings.tsx
@@ -7,7 +7,7 @@ function Settings() {
     return (
         <main className="main">
             <Card title="Settings">
-            <   div className="coming-soon-div">
+                <div className="coming-soon-div">
                     <img className="coming-soon-img" src={comingsoon} alt="coming soon" />
                 </div>
             </Card>
