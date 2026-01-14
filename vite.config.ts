16f2fd86ce2c03d2d61ef3de015e5501046566c3
sznsgthb <suzannehoftijzer@gmail.com>
2026-01-15 00:47:29 +0100

    router fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const base = process.env.DEPLOY_ENV === 'GH_PAGES' ? '/the-competence-chronicle/' : '/'
-
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base,
     optimizeDeps: {
         include: [
         "d3-array",
